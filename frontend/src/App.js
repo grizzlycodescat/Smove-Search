9f16723123f208006e2ff13855ba83be10322ce8
Unknown <jerome.papalie@gmail.com>
2018-08-10 15:49:12 +0800

    Removed App.Css
    Removed App.css, and started using component styling. Added margins to Global Styling;

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,52 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
-import "./App.css";
+import styled, { keyframes } from "styled-components";
+
+const Outer = styled.div`
+	text-align:center;
+`;
+
+const Header = styled("header")`
+	background-color: #223;
+	height: 150px;
+	padding: 20px;
+	color: white;
+
+	> h1 {
+		color: palevioletred;
+	}
+`;
+
+const spin = keyframes`
+	from { transform: rotate(0deg); }
+	to { transform: rotate(360deg); }
+`;
+
+const Logo = styled.img`
+	animation: ${spin} infinite 20s linear;
+	height: 80px;
+`;
+
+const Title = styled.h1`
+	font-size: 1.5em;
+`;
+
+const Intro = styled.p`
+	font-size: large;
+`;
 
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<h1 className="App-title">Welcome to React</h1>
-				</header>
-				<p className="App-intro">
+			<Outer>
+				<Header>
+					<Logo src={logo} alt="logo" />
+					<Title>Welcome to React</Title>
+				</Header>
+				<Intro>
           To get started, edit <code>src/App.js</code> and save to reload.
-				</p>
-			</div>
+				</Intro>
+			</Outer>
 		);
 	}
 }
